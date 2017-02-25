903fd89e96bb33c2424dce48956da18a6c5a184c
anmol jindal <anmol.jindal6199@gmail.com>
2017-02-25 10:30:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+var img = document.getElementById('madi');
+
+var marginleft =0
+
+function moveRight() {
+    marginLeft = marginleft + 10;
+	img.style.marginRight = marginleft;
+}
+
+img.onclick = function() {
+    var interval = setInterval(moveRight, 100);
+}
