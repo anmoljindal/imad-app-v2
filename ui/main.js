310e16b46a179497f737324e818398ca748f1df8
anmol jindal <anmol.jindal6199@gmail.com>
2017-02-25 19:56:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,34 @@ button.onclick = function() {
     request.open('GET', 'http://anmoljindal.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+var submit = document.getElementById('submit_btn');
+submit.onclick= function() {
+    
+    //create a requst object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200) {
+                //capture a list of names and render it as list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i < names.length; i++) {
+                    list = '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    //make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://anmoljindal.imad.hasura-app.io/submit-name=' + name,true);
+    request.send(null);
+}
