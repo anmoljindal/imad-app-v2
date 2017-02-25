4f7481532d4c26a2f5533a924c5e3f2b5547b304
anmol jindal <anmol.jindal6199@gmail.com>
2017-02-25 16:42:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,22 @@ var counter = 0;
 button.onclick = function() {
     
     //make request to the counter endpoint
-
-    //capture the response in the correct span
+    var request = new XMLHttpRequest();
     
-    //render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //capture the response in the correct span
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    
+    //Make the request
+    request.open('GET', 'http://anmoljindal.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
